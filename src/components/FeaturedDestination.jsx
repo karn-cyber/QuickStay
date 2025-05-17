d801d9bbca71ee0f49f7183f1c06a4e983606e88
karn-cyber <karnneelanshu@gmail.com>
2025-05-17 21:55:47 +0530

    Added Featured Destinations

---
+++ src/components/FeaturedDestination.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { roomsDummyData } from '../assets/assets'
+
+const FeaturedDestination = () => {
+  return (
+    <div>
+        <div>
+            {roomsDummyData.slice(0,4).map((room,index) => (
+                <HotelCard key={room._id} room={room} index={index}/>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedDestination
