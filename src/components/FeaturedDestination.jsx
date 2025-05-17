b8f1367f8a06b93974c7fb0b4a84d288fa234254
karn-cyber <karnneelanshu@gmail.com>
2025-05-17 22:04:28 +0530

    Fixed Css for the featured Section

--- src/components/FeaturedDestination.jsx
+++ src/components/FeaturedDestination.jsx
@@ -5,7 +5,7 @@ import HotelCard from './HotelCard'
 const FeaturedDestination = () => {
   return (
     <div>
-        <div>
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'> 
             {roomsDummyData.slice(0,4).map((room,index) => (
                 <HotelCard key={room._id} room={room} index={index}/>
             ))}
